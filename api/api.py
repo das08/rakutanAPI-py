fe94d28f6de76d30ed15c90e1c045cbdc8671a5e
das08 <takeda5955@gmail.com>
2020-09-24 18:45:14 +0900

    routing

--- api/api.py
+++ api/api.py
@@ -1,18 +1,47 @@
 from flask import Blueprint, jsonify
 
-
 app = Blueprint('api', __name__, url_prefix='/api')
 
 
-@app.route('/rakutan', methods=['GET'])
-def list_user():
-    return "hi"
-
-
+# 指定した講義ID(kid)のらくたん情報を取得する
 @app.route('/rakutan/<int:kid>', methods=['GET'])
-# kid: 講義ID
-def get_user(kid=None):
+def get_lecture(kid=None):
     return jsonify(kid)
+
+
+# 指定したユーザー(uid)の情報を取得する
+@app.route('/users/<str:uid>', methods=['GET'])
+# kid: 講義ID
+def get_users(uid=None):
+    return jsonify(uid)
+
+
+# 指定したユーザー(uid)の情報を作成する
+@app.route('/users', methods=['POST'])
+# kid: 講義ID
+def add_users():
+    return jsonify(uid)
+
+
+# 指定したユーザー(uid)の情報を更新する
+@app.route('/users/<str:uid>', methods=['PUT'])
+# kid: 講義ID
+def update_users(uid=None):
+    return jsonify(uid)
+
+
+# 指定した講義ID(kid)の過去問リンクを許可待ちリストに追加する
+@app.route('/kakomon', methods=['POST'])
+# kid: 講義ID
+def add_kakomon():
+    return jsonify(uid)
+
+
+# 指定した講義ID(kid)の過去問リンクを許可待ちリストから削除する
+@app.route('/kakomon/<int:kid>', methods=['DELETE'])
+# kid: 講義ID
+def delete_kakomon(kid=None):
+    return jsonify(uid)
 
 
 @app.errorhandler(404)
