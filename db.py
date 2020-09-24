00b31818f1a15fefb0eeb88aeb625815a4d7331d
das08 <takeda5955@gmail.com>
2020-09-24 12:01:21 +0900

    CRUD関連を実装

--- db.py
+++ db.py
@@ -4,24 +4,115 @@ import setting as env
 
 class Database:
     __dbIP = env.db_ip
+    __dbPort = env.db_port
     __dbUsername = env.db_username
     __dbPass = env.db_pass
+    __dbClient = None
+    __dbConn = None
 
-    def __init__(self, db_name, db_collection):
+    def __init__(self, db_name):
+        """
+        Initialization of Database
+        :param db_name: (str) database name
+        """
         self.__dbName = db_name
-        self.__dbCollection = db_collection
 
     def connect(self):
-        pass
+        """
+        Connect to mongodb
+        """
+        uri = f"mongodb://{self.__dbUsername}:{self.__dbPass}@{self.__dbIP}:{self.__dbPort}"
+        self.__dbClient = MongoClient(uri)
+        self.__dbConn = self.__dbClient[self.__dbName]
 
     def disconnect(self):
-        pass
+        """
+        Disconnect from mongodb
+        """
+        self.__dbClient.close()
 
-    def insert(self):
-        pass
+    def find(self, colName, query: dict, projection=None):
+        """
+        Perform find action
+        :param colName: (str) collection name
+        :param query: (dict) query
+        :param projection: (option) (dict) options to specify field
+        :return: (dict)
+        """
+        res = {
+            "result": None,
+            "count": None,
+            "queryResult": None
+        }
+        try:
+            collection = self.__dbConn[colName]
+            queryResults = collection.find(filter=query, projection=projection)
+            count = collection.count_documents(filter=query)
+            res.result = "success"
+            res.count = count
+            res.queryResult = queryResults
+        except Exception as e:
+            res.result = "exception"
+        finally:
+            return res
 
-    def update(self):
-        pass
+    def update(self, colName, query: dict):
+        """
+        Perform update action
+        :param colName: (str) collection name
+        :param query: (dict) query
+        :return: (dict)
+        """
+        res = {
+            "result": None,
+            "count": None,
+        }
+        try:
+            collection = self.__dbConn[colName]
+            count = collection.find_one_and_update(query)['count']
+            res.result = "success" if count > 0 else "fail"
+            res.count = count
+        except Exception as e:
+            res.result = "exception"
+        finally:
+            return res
 
-    def delete(self):
-        pass
+    def insert(self, colName, document: dict):
+        """
+        Perform insert action
+        :param colName: (str) collection name
+        :param document: (dict) document
+        :return: (dict)
+        """
+        res = {
+            "result": None,
+        }
+        try:
+            collection = self.__dbConn[colName]
+            count = collection.insert_one(document)
+            res.result = "success"
+        except Exception as e:
+            res.result = "exception"
+        finally:
+            return res
+
+    def delete(self, colName, query: dict):
+        """
+        Perform delete action
+        :param colName: (str) collection name
+        :param query: (dict) query
+        :return: (dict)
+        """
+        res = {
+            "result": None,
+            "count": None,
+        }
+        try:
+            collection = self.__dbConn[colName]
+            count = collection.delete_one(query).deleted_count
+            res.result = "success" if count > 0 else "fail"
+            res.count = count
+        except Exception as e:
+            res.result = "exception"
+        finally:
+            return res
