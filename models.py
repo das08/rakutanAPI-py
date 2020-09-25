e95375790056346d724216f03feca1d8395ef386
das08 <takeda5955@gmail.com>
2020-09-25 21:49:18 +0900

    add classmethod to convert to Rakutan object

--- models.py
+++ models.py
@@ -18,6 +18,26 @@ class Rakutan:
         self.total: list = t
         self.url: str = u
 
+    @classmethod
+    def from_dict(cls, dbRakutanDict: dict):
+        return Rakutan(
+            lecID=dbRakutanDict['id'],
+            fN=dbRakutanDict['facultyname'],
+            lN=dbRakutanDict['lecturename'],
+            g=dbRakutanDict['groups'],
+            c=dbRakutanDict['credits'],
+            a=[dbRakutanDict['accept_prev'], dbRakutanDict['accept_prev2'], dbRakutanDict['accept_prev3']],
+            t=[dbRakutanDict['total_prev'], dbRakutanDict['total_prev2'], dbRakutanDict['total_prev3']],
+            u=dbRakutanDict['url']
+        )
+
+    @classmethod
+    def from_list(cls, dbRakutanList: list):
+        rakutanList = []
+        for rakutan in dbRakutanList:
+            rakutanList.append(cls.from_dict(rakutan))
+        return rakutanList
+
     def to_dict(self):
         return {
             'lecID': self.lecID,
