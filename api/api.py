86abfb2770b969e62688044506a6f8c401c90a2e
das08 <takeda5955@gmail.com>
2020-09-25 21:48:12 +0900

    routing

--- api/api.py
+++ api/api.py
@@ -5,12 +5,19 @@ app = Blueprint('api', __name__, url_prefix='/api')
 
 # 指定した講義ID(kid)のらくたん情報を取得する
 @app.route('/rakutan/<int:kid>', methods=['GET'])
-def get_lecture(kid=None):
+def get_lecture_by_id(kid=None):
     return jsonify(kid)
 
 
+# 指定した講義ID(kid)のらくたん情報を取得する
+@app.route('/rakutan/search/<search_word>', methods=['GET'])
+def get_lecture_by_search_word(search_word=None):
+    search_word = search_word.strip().replace('％', '%')
+    return jsonify(search_word)
+
+
 # 指定したユーザー(uid)の情報を取得する
-@app.route('/users/<str:uid>', methods=['GET'])
+@app.route('/users/<uid>', methods=['GET'])
 # kid: 講義ID
 def get_users(uid=None):
     return jsonify(uid)
@@ -20,11 +27,11 @@ def get_users(uid=None):
 @app.route('/users', methods=['POST'])
 # kid: 講義ID
 def add_users():
-    return jsonify(uid)
+    return jsonify()
 
 
 # 指定したユーザー(uid)の情報を更新する
-@app.route('/users/<str:uid>', methods=['PUT'])
+@app.route('/users/<uid>', methods=['PUT'])
 # kid: 講義ID
 def update_users(uid=None):
     return jsonify(uid)
@@ -34,14 +41,14 @@ def update_users(uid=None):
 @app.route('/kakomon', methods=['POST'])
 # kid: 講義ID
 def add_kakomon():
-    return jsonify(uid)
+    return jsonify()
 
 
 # 指定した講義ID(kid)の過去問リンクを許可待ちリストから削除する
-@app.route('/kakomon/<int:kid>', methods=['DELETE'])
+@app.route('/kakomon/<kid>', methods=['DELETE'])
 # kid: 講義ID
 def delete_kakomon(kid=None):
-    return jsonify(uid)
+    return jsonify(kid)
 
 
 @app.errorhandler(404)
